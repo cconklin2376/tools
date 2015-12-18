c7353033bc0ff36451703bf7d2d36c9d04dd7c85
cconklin2376 <cconklin2376@gmail.com>
2015-12-18 09:35:49 -0500

    Add date to output. Remove status code from output. Add newlines.

--- check_hosts.py
+++ check_hosts.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
 import os
+import datetime
 
 ips= []
 netwk = "192.168.0."
 minip = 1
-maxip = 23
+maxip = 25
+now = datetime.datetime.now().strftime("%h %d")
 
-
-print("Checking network " + netwk + " hosts(" + str(minip) + " - " + str(maxip))
+print("Checking network " + netwk + " hosts(" + str(minip) + " - " + str(maxip) + ") on " + now + " \n\n")
 for val in range(minip, maxip):
 	hostname = netwk + str(val)
 	ret = os.system("ping -c 1 " + hostname + " > /dev/null 2>&1")
 	if ret == 0:
-		print hostname + " : " + str(ret)
+		print hostname + " is online.\n"
 
